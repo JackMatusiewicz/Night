a9a027a865b2e37f07debedf1000b9d5afd2e002
JackMatusiewicz <jack.matusiewicz@gmail.com>
2018-05-21 23:28:27 +0100

    Adding logic to actually force a logOff event

---
+++ Night/Control.fs
@@ -0,0 +1,24 @@
+namespace Night
+
+open System
+open System.Runtime.InteropServices;
+
+type ExitMethod =
+    | LogOff
+    | ShutDown
+
+module Control =
+
+    [<DllImport("user32.dll", SetLastError = true)>]
+    extern [<MarshalAs(UnmanagedType.Bool)>] bool ExitWindowsEx(uint32 uFlags, uint32 dwReason);
+
+    let toUint (method : ExitMethod) =
+        match method with
+        | LogOff -> 0x00
+        | ShutDown -> 0x01
+
+    let exit (method : ExitMethod) =
+        let userDefinedShutdown = (uint32 0x40000000)
+        let methodUint = toUint method
+        let forceMethod = methodUint ||| 0x04
+        ExitWindowsEx ((uint32 forceMethod), userDefinedShutdown)
--- Night/Program.fs
+++ Night/Program.fs
@@ -1,4 +1,8 @@
-﻿[<EntryPoint>]
+﻿open Night
+
+[<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    printfn "Press enter to log off"
+    System.Console.ReadLine ()
+    Control.exit LogOff
     0 // return an integer exit code
